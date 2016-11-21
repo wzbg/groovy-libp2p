a5607a01a79cb3a8501d7c9f19f02db2ef80477b
￠幻冰 <huanbing@wacai.com>
2016-11-21 17:39:17 +0800

    PeerInfo MultiAddr

--- src/main/groovy/xyz/urac/libp2p/peer/PeerInfo.groovy
+++ src/main/groovy/xyz/urac/libp2p/peer/PeerInfo.groovy
@@ -2,6 +2,8 @@ package xyz.urac.libp2p.peer
 
 import groovy.transform.Canonical
 
+import org.ipfs.api.MultiAddress
+
 /**
  * Peer represents a peer on the IPFS network
  * 
@@ -11,12 +13,41 @@ import groovy.transform.Canonical
 @Canonical
 class PeerInfo {
   PeerId id
+  List<MultiAddress> multiAddrs = []
 
   PeerInfo(PeerId peerId) {
     assert peerId != null : 'Missing peerId. Use PeerId.create([bits]) to create one'
     id = peerId
   }
-  
+
+  def addMultiAddr(addr) {
+    if (!addr instanceof MultiAddress) {
+      addr = new MultiAddress(addr)
+    }
+    if (multiAddrs.contains(addr)) {
+      return true
+    }
+    multiAddrs.add addr
+  }
+
+  def removeMultiAddr(addr) {
+    if (!addr instanceof MultiAddress) {
+      addr = new MultiAddress(addr)
+    }
+    multiAddrs.remove addr
+  }
+
+  void replaceMultiAddrs(existing, fresh) {
+    if (existing instanceof MultiAddress) {
+      existing = [existing]
+    }
+    if (fresh instanceof MultiAddress) {
+      fresh = [fresh]
+    }
+    existing.forEach { removeMultiAddr it }
+    fresh.forEach { removeMultiAddr it }
+  }
+
   /**
    * generation
    */
--- src/test/groovy/xyz/urac/libp2p/peer/PeerInfoTests.groovy
+++ src/test/groovy/xyz/urac/libp2p/peer/PeerInfoTests.groovy
@@ -1,9 +1,20 @@
 package xyz.urac.libp2p.peer
 
+import org.ipfs.api.MultiAddress
+
 class PeerInfoTests extends GroovyTestCase {
-  void testCreate() {
+  void test() {
     def peerInfo = PeerInfo.create()
     assert peerInfo != null
     println peerInfo
+    MultiAddress addr = new MultiAddress('')
+    def result = peerInfo.addMultiAddr addr
+    assert result
+    result = peerInfo.removeMultiAddr addr
+    assert result
+    peerInfo.replaceMultiAddrs(addr, addr)
+    peerInfo.replaceMultiAddrs([addr], addr)
+    peerInfo.replaceMultiAddrs(addr, [addr])
+    peerInfo.replaceMultiAddrs([addr], [addr])
   }
 }
