d38d5bd09a3dd8cb36286295baa9df979e6d714b
unrealce <z35305515@gmail.com>
2016-11-24 01:50:42 +0800

    regex

--- src/main/groovy/xyz/urac/libp2p/mafmt/Pattern.groovy
+++ src/main/groovy/xyz/urac/libp2p/mafmt/Pattern.groovy
@@ -16,24 +16,36 @@ import xyz.urac.libp2p.util.Operator
  */
 @AutoClone
 class Pattern {
+  String regex
   List<Base> bases
-  List<Operator> ops = []
 
   Pattern(String proto) {
     assert proto != null : 'invalid proto provided'
-    bases = [new Base(proto)]
+    bases = [new Base(regex = proto)]
   }
 
   private op(Pattern other, Operator op) {
     def result = this.clone()
     result.with {
-      bases += other.bases
-      ops << op
-      if (other.ops) {
-        ops << Operator.leftParentheses
-        ops += other.ops
-        ops << Operator.rightParentheses
+      def regex1 = regex
+      def regex2 = other.regex
+      if (bases.size() > 1) {
+        regex1 = "($regex1)"
       }
+      switch (op) {
+        case Operator.or:
+          if (other.bases.size() > 1) {
+            regex2 = "($regex2)"
+          }
+          regex = "$regex1|$regex2"
+          break
+        case Operator.and:
+          regex = "$regex1/$regex2"
+          break
+        default:
+          throw new IllegalStateException('unrecognized pattern op!')
+      }
+      bases += other.bases
     }
     result
   }
@@ -74,46 +86,12 @@ class Pattern {
   def matches(addr) {
     def protos = protos addr
     if (!protos) return false
-    def bases = bases.clone()
-    def ops = ops.clone()
-    def base = bases.pop()
-    while (ops) {
-      switch (ops.pop()) {
-        case Operator.or:
-          if (base.partialMatch(protos)) return true
-        case Operator.and:
-          if (!base.partialMatch(protos)) return false
-        case Operator.leftParentheses:
-        case Operator.rightParentheses:
-          break
-        default:
-          throw new IllegalStateException('unrecognized pattern op!')
-      }
-      base = bases.pop()
-    }
-    base.partialMatch protos
+    println "${protos.join('/')} =~ $regex ${protos.join('/') ==~ ~regex}" // test
+    protos.join('/') ==~ ~regex
   }
-  
+
   @Override
   public String toString() {
-    String result = bases.head()
-    def ops = ops.clone()
-    bases.tail()?.each {
-      def op1 = ops.head()
-      ops = ops.tail()
-      def op2 = ops ? ops.first() : null
-      switch (op2) {
-        case Operator.leftParentheses:
-          result += op1.sign + op2.sign + it
-          ops = ops.tail()
-          break
-        case Operator.rightParentheses:
-          result += op1.sign + it + op2.sign
-          ops = ops.tail()
-          break
-        default: result += op1.sign + it
-      }
-    }
-    result
+    regex
   }
 }
--- src/test/groovy/xyz/urac/libp2p/mafmt/PatternTests.groovy
+++ src/test/groovy/xyz/urac/libp2p/mafmt/PatternTests.groovy
@@ -1,13 +1,13 @@
 package xyz.urac.libp2p.mafmt
 
 class PatternTests extends GroovyTestCase {
-  static IP = new Pattern('ip4') | new Pattern('ip6')
-  static TCP = IP & new Pattern('tcp')
-  static UDP = IP & new Pattern('udp')
-  static UTP = UDP & new Pattern('utp')
-  static Unreliable = UDP
-  static Reliable = TCP | UTP
-  static IPFS = Reliable & new Pattern('ipfs')
+  static IP = new Pattern('ip4') | new Pattern('ip6') // ~'ip4|ip6'
+  static TCP = IP & new Pattern('tcp') // ~'(ip4|ip6)/tcp'
+  static UDP = IP & new Pattern('udp') // ~'(ip4|ip6)/udp'
+  static UTP = UDP & new Pattern('utp') // ~'(ip4|ip6)/udp/utp'
+  static Unreliable = UDP // ~'(ip4|ip6)/udp'
+  static Reliable = TCP | UTP // ~'((ip4|ip6)/tcp)|((ip4|ip6)/udp/utp)'
+  static IPFS = Reliable & new Pattern('ipfs') // ~'(((ip4|ip6)/tcp)|((ip4|ip6)/udp/utp))/ipfs'
 
   def assertMatches(p, args, b = true) {
     args?.each {
