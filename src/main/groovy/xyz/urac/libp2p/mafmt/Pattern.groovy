aead31fefa61a6ac43015c7ab5dd02ed3218b347
￠幻冰 <huanbing@wacai.com>
2016-11-23 15:30:33 +0800

    Pattern toString()

--- src/main/groovy/xyz/urac/libp2p/mafmt/Pattern.groovy
+++ src/main/groovy/xyz/urac/libp2p/mafmt/Pattern.groovy
@@ -1,7 +1,6 @@
 package xyz.urac.libp2p.mafmt
 
 import groovy.transform.AutoClone
-import groovy.transform.Canonical
 
 import org.ipfs.api.MultiAddress
 import org.ipfs.api.Protocol
@@ -16,7 +15,6 @@ import xyz.urac.libp2p.util.Operator
  * @date 2016年11月22日 下午5:23:23
  */
 @AutoClone
-@Canonical
 class Pattern {
   List<Base> bases
   List<Operator> ops = []
@@ -76,20 +74,46 @@ class Pattern {
   def matches(addr) {
     def protos = protos addr
     if (!protos) return false
-    def base = bases.pop()
-    while (ops) {
-      switch (ops.pop()) {
-        case or:
-          if (base.partialMatch(protos)) return true
-        case and:
-          if (!base.partialMatch(protos)) return false
-        case leftParentheses:
-        case rightParentheses:
-        default:
-          throw new IllegalStateException('unrecognized pattern op!')
+    this.clone().with {
+      def base = bases.pop()
+      while (ops) {
+        switch (ops.pop()) {
+          case Operator.or:
+            if (base.partialMatch(protos)) return true
+          case Operator.and:
+            if (!base.partialMatch(protos)) return false
+          case Operator.leftParentheses:
+          case Operator.rightParentheses:
+            break
+          default:
+            throw new IllegalStateException('unrecognized pattern op!')
+        }
+        base = bases.pop()
       }
-      base = bases.pop()
     }
     base.partialMatch protos
+  }
+  
+  @Override
+  public String toString() {
+    String result = bases.head()
+    def ops = ops.clone()
+    bases.tail()?.each {
+      def op1 = ops.head()
+      ops = ops.tail()
+      def op2 = ops ? ops.first() : null
+      switch (op2) {
+        case Operator.leftParentheses:
+          result += op1.sign + op2.sign + it
+          ops = ops.tail()
+          break
+        case Operator.rightParentheses:
+          result += op1.sign + it + op2.sign
+          ops = ops.tail()
+          break
+        default: result += op1.sign + it
+      }
+    }
+    result
   }
 }
--- src/test/groovy/xyz/urac/libp2p/mafmt/PatternTests.groovy
+++ src/test/groovy/xyz/urac/libp2p/mafmt/PatternTests.groovy
@@ -3,18 +3,18 @@ package xyz.urac.libp2p.mafmt
 class PatternTests extends GroovyTestCase {
   void testOps() {
     final IP = new Pattern('ip4') | new Pattern('ip6')
-    println "IP: $IP"
     final TCP = IP & new Pattern('tcp')
-    println "TCP: $TCP"
     final UDP = IP & new Pattern('udp')
-    println "UDP: $UDP"
     final UTP = UDP & new Pattern('utp')
-    println "UTP: $UTP"
     final Unreliable = UDP
-    println "Unreliable: $Unreliable"
     final Reliable = TCP | UTP
-    println "Reliable: $Reliable"
     final IPFS = Reliable & new Pattern('ipfs')
+    println "IP: $IP"
+    println "TCP: $TCP"
+    println "UDP: $UDP"
+    println "UTP: $UTP"
+    println "Unreliable: $Unreliable"
+    println "Reliable: $Reliable"
     println "IPFS: $IPFS"
   }
 }
